148f891e71abf1d8739f5a3f199f6e6e424e22aa
brunommbruno <brunommbruno@gmail.com>
2021-06-11 00:29:17 +0200

    added navbar (no links)

---
+++ src/components/Navbar.js
@@ -0,0 +1,13 @@
+const Navbar = () => {
+  return (
+    <div className="main-navbar">
+      <a href="asd">HOME</a>
+      <a href="asd">ABOUT</a>
+      <a href="asd">SKILLS</a>
+      <a href="asd">PROJECTS</a>
+      <a href="asd">CONTACTS</a>
+    </div>
+  );
+};
+
+export default Navbar;
