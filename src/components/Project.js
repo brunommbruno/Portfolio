60125be14a18a65464c240fa57b2e2a78dd9fe07
brunommbruno <brunommbruno@gmail.com>
2021-06-11 13:30:42 +0200

    added zoom in styling to cards

--- src/components/Project.js
+++ src/components/Project.js
@@ -4,7 +4,9 @@ const Project = ({ title, image, link, code }) => {
   return (
     <div className="project">
       <Card style={{ width: "20rem" }}>
-        <Card.Img variant="top" src={image} />
+        <a href={link}>
+          <Card.Img variant="top" src={image} />
+        </a>
         <Card.Body>
           <Card.Title>{title}</Card.Title>
           <Card.Link href={link}>{title}</Card.Link>
